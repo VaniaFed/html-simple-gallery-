6e9f83d54a399a8c73cc9017210708529cb62da1
VaniaFed <ztankist@gmail.com>
2019-06-18 09:09:57 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,156 @@
+(() => {
+  const set = [
+    [
+      {
+        url: 'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1521913626209-0fbf68f4c4b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1436262513933-a0b06755c784?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1464316325666-63beaf639dbb?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1519091830432-41608a52904e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1495107014100-1472b7b6f00f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1505356822725-08ad25f3ffe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1512654755163-0f342db4466f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+    ],
+    [
+      {
+        url: 'https://images.unsplash.com/photo-1526234172573-67cabe4ebc80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1542834759-4db91f2574d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1530533718754-001d2668365a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1520176501380-9a174bf7c783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1494252713559-f26b4bf0b174?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1507491742813-59efde079449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1465403843003-b277b46a1120?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1477854378975-90f49cf366df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+    ],
+    [
+      {
+        url: 'https://images.unsplash.com/photo-1457419655743-2e0acc6b5122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1518366352543-6609db5ccdef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1498144846853-60ca2d43853b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1514388614019-c755c395185c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1496858705185-1f25b056e4a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        alt: '',
+      },
+    ],
+  ];
+  let images;
+
+  const container = document.querySelector('.gallery__side_chooseImage');
+  const currentImageClassName = 'gallery__currentImage';
+
+  const addPictureToContainer = (inputImage, container) => {
+    const newImg = document.createElement('img');
+    newImg.src = inputImage.url;
+    newImg.alt = inputImage.alt;
+    newImg.classList.add('gallery__image');
+
+    container.appendChild(newImg);
+  };
+
+  const changeImage = image => {
+    const currentImage = document.querySelector('.gallery__currentImage');
+    currentImage.src = image.src;
+  }
+
+  const init = () => {
+    images = set[0];
+
+    renderImages(images);
+
+    let htmlImages = document.querySelectorAll('.gallery__image');
+    changeImage(htmlImages[0], currentImageClassName);
+    for(let i = 0; i < htmlImages.length; i++) {
+      htmlImages[i].addEventListener('click', () => {
+        changeImage(htmlImages[i], currentImageClassName);
+        console.log(htmlImages[i]);
+      });
+    }
+  }
+  const renderImages = images => {
+    container.innerHTML = '';
+    images.map(image => {
+      addPictureToContainer(image, container);
+    });
+  };
+
+  init();
+
+  const links = document.querySelectorAll('.nav__link');
+  for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', () => {
+      images = set[i];
+      renderImages(images);
+
+      htmlImages = document.querySelectorAll('.gallery__image');
+      changeImage(htmlImages[0], currentImageClassName);
+      for (let j = 0; j < htmlImages.length; j++) {
+        htmlImages[j].addEventListener('click', () => {
+          changeImage(htmlImages[j], currentImageClassName);
+          console.log(htmlImages[j]);
+        });
+      }
+    }) 
+  }
+})();
